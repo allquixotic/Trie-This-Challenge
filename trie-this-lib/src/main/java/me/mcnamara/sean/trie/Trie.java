8dff389fd947c7ceed612301c0e2b2d01f9f3574
Sean McNamara <smcnam@gmail.com>
2018-03-14 17:34:54 -0400

    Minimally functional trie (TDD tested).

--- trie-this-lib/src/main/java/me/mcnamara/sean/trie/Trie.java
+++ trie-this-lib/src/main/java/me/mcnamara/sean/trie/Trie.java
@@ -1,7 +1,10 @@
 package me.mcnamara.sean.trie;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Trie {
 	private TrieNode root = new TrieNode("", false);
@@ -41,7 +44,49 @@ public class Trie {
 
 	public List<String> search(String prefix) {
 		List<String> retval = new ArrayList<String>();
-
+		
+		TrieNode node = root;
+		//First navigate down the tree to the end of the prefix
+		for(int i = 0; i < prefix.length(); i++) {
+			String subprefix = String.valueOf(prefix.charAt(i));
+			TrieNode tmp = node.getChild(subprefix);
+			if(tmp == null) {
+				//There's nothing with this prefix in the tree
+				return retval;
+			}
+			node = tmp;
+		}
+		
+		//Now do a DFS traversal from this node on all nodes and construct words
+		StringBuilder sb = new StringBuilder(prefix.substring(0, prefix.length() - 1));
+		List<TrieNode> visitedNodes = new LinkedList<>();
+		List<TrieNode> unvisitedNodes = new LinkedList<>();
+		unvisitedNodes.add(node);
+		
+		while(!unvisitedNodes.isEmpty()) {
+			node = unvisitedNodes.get(0);
+			
+			//Add all children not already visited to the list
+            List<TrieNode> newNodes = node.getChildren()
+                    .values().stream().filter(n -> !visitedNodes.contains(n)).collect(Collectors.toList());
+            unvisitedNodes.addAll(0, newNodes);
+			
+			//Process the current node
+            if(!visitedNodes.contains(node)) {
+				sb.append(node.getValue());
+				if(node.isWord()) {
+					retval.add(sb.toString());
+				}
+				visitedNodes.add(node);
+            }
+            
+            //Delete the current node from the buffer and the stack
+            if(newNodes.size() == 0) {
+            		sb.deleteCharAt(sb.length() - 1);
+            		unvisitedNodes.remove(0);
+            }
+		}
+		
 		return retval;
 	}
 }
--- trie-this-lib/src/test/java/me/mcnamara/sean/trie/tests/TrieTest.java
+++ trie-this-lib/src/test/java/me/mcnamara/sean/trie/tests/TrieTest.java
@@ -2,6 +2,7 @@ package me.mcnamara.sean.trie.tests;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.junit.Before;
@@ -10,7 +11,7 @@ import org.junit.Test;
 import me.mcnamara.sean.trie.Trie;
 
 /**
- * Unit test for Trie.
+ * Unit test suite for Trie class with prefix search functionality.
  */
 public class TrieTest
 {
@@ -21,41 +22,80 @@ public class TrieTest
     		this.trie = new Trie();
     }
     
+    //****Unit tests for explicitly stated requirements.****
+    
+    //Requirement: Adding a word returns true.
     @Test
     public void testAddWordEmptyTree_returnsTrue() {
     		assertThat(trie.add("hello")).isTrue();
     }
     
+    //Requirement: Adding duplicate word returns false;
     @Test
     public void testAddDuplicateWord_returnsFalse() {
     		assertThat(trie.add("hello")).isTrue();
     		assertThat(trie.add("hello")).isFalse();
     }
     
+    //Requirement: Contains returns true if word is in the trie.
     @Test
     public void testContainsLookForExistingWord_returnsTrue() {
     		trie.add("hello");
     		assertThat(trie.contains("hello")).isTrue();
     }
     
+    //Requirement: Contains returns false if word doesn't exist in the trie. 
+    //This test also makes sure it doesn't return true just because the input is a prefix of a word that exists.
     @Test
     public void testContainsLookForNonWord_returnsFalse() {
     		trie.add("hello");
     		assertThat(trie.contains("hel")).isFalse();
     }
     
+    //Requirement: no results for search prefix that doesn't exist.
     @Test
     public void testSearchZeroResults() {
-    		List.of("hello", "hellothere", "hellfire", "hem", "hew", "hi", "bye").forEach((word) -> trie.add(word));		
+    		List.of("hello", "hellothere", "hellfire", "hem", "hew", "hi", "bye").forEach(word -> trie.add(word));		
 		List<String> words = trie.search("ha");
 		assertThat(words.size()).isEqualTo(0);
     }
     
+    //Requirement: happy path search.
     @Test
     public void testSearchThreeResults() {
-    		List.of("hello", "hellothere", "hellfire", "hem", "hew", "hi", "bye").forEach((word) -> trie.add(word));    		
+    		List.of("hello", "hellothere", "hellfire", "hem", "hew", "hi", "bye").forEach(word -> trie.add(word));    		
     		List<String> words = trie.search("hel");
-    		List.of("hello", "hellothere", "hellfire").forEach((word) -> assertThat(words.contains(word)).isTrue());
     		assertThat(words.size()).isEqualTo(3);
+    		List.of("hello", "hellothere", "hellfire").forEach(word -> assertThat(words.contains(word)).isTrue());
+    }
+    
+    //Extra requirement: output list should be sorted.
+    @Test
+    public void testSearchResultSortOrder() {
+    		List.of("azalia", "aardvark", "abacus", "alchemy", "adagio", "hem", "hew", "hi", "bye", "acquiesce").forEach(word -> trie.add(word));    		
+    		List<String> words = trie.search("a");
+    		assertThat(words.size()).isEqualTo(6);
+    		List<String> expectedList = List.of("aardvark", "abacus", "acquiesce", "adagio", "alchemy", "azalia");
+    		int i = 0; 
+    		for(String expected : expectedList) {
+    			assertThat(words.get(i++)).isEqualTo(expected);
+    		}
+    }
+    
+    //****Acceptance Tests - look for odd behavior and potential bugs.****
+    
+    //Make sure casing doesn't affect the search results.
+    @Test
+    public void testCasing() {
+    		List<String> inputs = List.of("azalia", "AARDVARK", "Abacus", "aLcHeMy");
+    		List<String> lowerInputsSorted = List.of("aardvark", "abacus", "alchemy", "azalia");
+    		inputs.forEach(word -> assertThat(trie.add(word)).isTrue());
+    		
+    		List<String> search = trie.search("a");
+    		List<String> searchUpper = trie.search("A");
+    		
+    		inputs.forEach(word -> assertThat(trie.contains(word)));
+    		assertThat(search.equals(searchUpper));
+    		assertThat(search.equals(lowerInputsSorted));
     }
 }
